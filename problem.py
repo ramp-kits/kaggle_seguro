06424beb215ca056bb3e241cd4d914f503cd9a34
Balazs Kegl <balazs.kegl@gmail.com>
2017-10-26 20:36:20 +0200

    initial commit

---
+++ problem.py
@@ -0,0 +1,57 @@
+import os
+import numpy as np
+import pandas as pd
+import rampwf as rw
+from sklearn.model_selection import StratifiedShuffleSplit
+
+problem_title = 'Kaggle Porto-Seguro safe driver prediction'
+_target_column_name = 'target'
+_prediction_label_names = [0, 1]
+# A type (class) which will be used to create wrapper objects for y_pred
+Predictions = rw.prediction_types.make_multiclass(
+    label_names=_prediction_label_names)
+# An object implementing the workflow
+workflow = rw.workflows.FeatureExtractorClassifier()
+
+score_types = [
+    rw.score_types.NormalizedGini(name='ngini', precision=3),
+    rw.score_types.ROCAUC(name='auc', precision=3),
+    rw.score_types.Accuracy(name='acc', precision=3),
+    rw.score_types.NegativeLogLikelihood(name='nll', precision=3),
+]
+
+
+def get_cv(X, y):
+    cv = StratifiedShuffleSplit(n_splits=8, test_size=0.2, random_state=57)
+    return cv.split(X, y)
+
+
+def get_train_data(path='.'):
+    test = os.getenv('RAMP_TEST_MODE', 0)
+    if test:
+        data = pd.read_csv(os.path.join(path, 'data', 'train.csv'), nrows=100)
+    else:
+        data = pd.read_csv(os.path.join(path, 'data', 'train.csv'))
+    y_array = data[_target_column_name].values
+    X_df = data.drop(_target_column_name, axis=1)
+    return X_df, y_array
+
+
+def get_test_data(path='.'):
+    test = os.getenv('RAMP_TEST_MODE', 0)
+    if test:
+        X_df = pd.read_csv(os.path.join(path, 'data', 'test.csv'), nrows=100)
+    else:
+        X_df = pd.read_csv(os.path.join(path, 'data', 'test.csv'))
+    y_array = np.zeros(len(X_df))  # dummy labels for syntax
+    y_array[0] = 1  # to make AUC work
+    return X_df, y_array
+
+
+def save_y_pred(y_pred_test, path='.'):
+    sample_df = pd.read_csv(os.path.join(
+        path, 'data', 'sample_submission.csv'))
+    df = pd.DataFrame()
+    df['id'] = sample_df['id']
+    df['target'] = y_pred_test[:, 1]
+    df.to_csv('y_pred_test.csv', index=False)
---
+++ submissions/starting_kit/feature_extractor.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+class FeatureExtractor():
+    def __init__(self):
+        pass
+
+    def fit(self, X_df, y):
+        pass
+
+    def transform(self, X_df):
+        return X_df.values
