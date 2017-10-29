532489433f3d1ce8512ba97edb33bcdeaa6c0dbd
Balazs Kegl <balazs.kegl@gmail.com>
2017-10-29 20:06:16 +0100

    add mock data

--- mock_data.py
+++ mock_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-train_filename = 'data/train.csv'
+train_filename = 'kaggle_data/train.csv'
 data = pd.read_csv(train_filename)
 size = 500
 mock_data = data.iloc[:500]
@@ -11,6 +11,6 @@ mock_data_train = mock_data.iloc[:400]
 mock_data_test = mock_data.iloc[400:]
 mock_data_sample_submission = mock_data_test[['id', 'target']]
 mock_data_test = mock_data_test.drop('target', axis=1)
-mock_data_train.to_csv('mock_data/train.csv')
-mock_data_test.to_csv('mock_data/test.csv')
-mock_data_sample_submission.to_csv('mock_data/sample_submission.csv')
+mock_data_train.to_csv('data/train.csv')
+mock_data_test.to_csv('data/test.csv')
+mock_data_sample_submission.to_csv('data/sample_submission.csv')
